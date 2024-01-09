3ee056c45b9efbf3693afe6a96716ac0f9c87069
Saso Kiselkov <skiselkov@gmail.com>
2024-01-09 17:08:47 +0100

    Added null checks to NonNull, as strange as that may seem, to add runtime checks to partial compile-time checks from C compilers.

--- src/c_ptr.rs
+++ src/c_ptr.rs
@@ -7,6 +7,8 @@
  * Copyright 2024 Saso Kiselkov. All rights reserved.
  */
 
+use std::ptr::NonNull;
+
 pub trait SafePointers<T> {
 	fn as_ref_safe(self) -> Option<&'static T>;
 	fn as_ref_unwrap<'a>(self) -> &'a T;
@@ -82,5 +84,35 @@ impl<T> SafePointerSlicesMut<T> for *mut T {
 		unsafe {
 			std::slice::from_raw_parts_mut(ptr, len)
 		}
+	}
+}
+
+pub trait SafePointerNonNull<T> {
+	fn as_ref_unwrap<'a>(self) -> &'a T;
+	fn as_mut_unwrap<'a>(self) -> &'a mut T;
+	fn as_ptr_unwrap(self) -> *mut T;
+}
+
+impl<T> SafePointerNonNull<T> for NonNull<T> {
+	fn as_ref_unwrap<'a>(self) -> &'a T {
+		unsafe {
+			self.as_ptr()
+			    .as_ref()
+			    .expect("Attempted to access a NULL pointer")
+		}
+	}
+	fn as_mut_unwrap<'a>(self) -> &'a mut T {
+		unsafe {
+			self.as_ptr()
+			    .as_mut()
+			    .expect("Attempted to access a NULL pointer")
+		}
+	}
+	fn as_ptr_unwrap(self) -> *mut T {
+		let ptr = self.as_ptr();
+		if ptr.is_null() {
+			panic!("Attempted to access a NULL pointer");
+		}
+		ptr
 	}
 }
